6621b2aabd1d63138b951b56c2b26c524494bb71
Michael Mackenzie <mike.a.mackenzie@gmail.com>
2019-03-08 15:48:34 -0500

    solutions up to #8

--- src/project_euler_clojure/core.clj
+++ src/project_euler_clojure/core.clj
@@ -1,10 +1,21 @@
+;;****************************************************************************
+;; Solutions to problems to Project Euler (https://projecteuler.net/)
+;; in Clojure.
+;;
+;; Author: Michael Mackenzie <mike.a.mackenzie@gmail.com>
+;;****************************************************************************
+
+
 (ns project-euler-clojure.core
+  (:require [clojure.math.combinatorics :as combo]
+            [clojure.tools.namespace.repl :refer [refresh]]
+            [clojure.string :as str])
   (:gen-class))
 
 
-;============================================================================
-; Helper functions
-;============================================================================
+;;============================================================================
+;; Helper functions
+;;============================================================================
 
 (defn fibonacci
   "Return a lazy sequence of Fibonacci numbers"
@@ -82,8 +93,8 @@
   [factorizations]
   (loop [max-deg-factors (sorted-map)
 
-         ; flattens a single level, giving us a sequence of all factors
-         ; and degrees of all factorizations
+         ;; flattens a single level, giving us a sequence of all factors
+         ;; and degrees of all factorizations
          remaining (mapcat identity factorizations)]
 
     (if (empty? remaining)
@@ -95,12 +106,37 @@
         (recur (assoc max-deg-factors curr-factor max-degree)
                (rest remaining))))))
 
+(defn digit
+  "Get the nth digit of x (zero-indexed, ascending).
+  i.e.  for the number 123 we have
+        (digit 123 0) = 3, (digit 123 1) = 2, (digit 123 2) = 1"
+  [x n]
+  (let [pow-of-10 (int (Math/pow 10 (inc n)))]
+    (/ (- (rem x pow-of-10) (rem x (/ pow-of-10 10))) (/ pow-of-10 10))))
 
-;============================================================================
-; Solutions to problems
-;============================================================================
+(defn digit-count
+  "Returns the number of digits of x"
+  [x]
+  (int (Math/ceil (logarithm 10 x))))
 
-(def problem-1
+(defn palindrome?
+  "Determine if a given number is a palindrome.
+  90009 is a palindrome, but 123 is not."
+  [x]
+  (case x
+    0 true                                                  ;; avoid division by 0
+    (let [n-digits (digit-count x)]
+      (every?
+        true?
+        (map (fn [y]
+               (= (digit x y) (digit x (- (dec n-digits) y))))
+             (range n-digits))))))
+
+;;============================================================================
+;; Solutions to problems
+;;============================================================================
+
+(defn problem-1 []
   (letfn [(inner [current count]
             (if (= current 1000)
               count
@@ -111,14 +147,34 @@
                 (recur (inc current) count))))]
     (inner 1 0)))
 
-(def problem-2
+(defn problem-2 []
   (reduce +
           (filter even?
                   (take-while (partial > 4000000) (fibonacci)))))
 
-(def problem-3 (last (prime-factors 600851475143)))
+(defn problem-3 []
+  (last (prime-factors 600851475143)))
 
-(def problem-4
+;; A palindromic number reads the same both ways.
+;; The largest palindrome made from the product of two 2-digit numbers
+;; is 9009 = 91 × 99.
+;;
+;; Find the largest palindrome made from the product of two 3-digit numbers.
+(defn problem-4 []
+  (let [nums-with-3-digits (range 100 1000)]
+    (apply max
+           (filter palindrome?
+                   (map #(apply * %)
+                        (combo/cartesian-product nums-with-3-digits
+                                                 nums-with-3-digits))))))
+
+
+(defn problem-5 []
+  (reduce *
+          (map (fn [x] (int (apply #(Math/pow %1 %2) x)))
+               (merge-factorizations (map prime-factorization (range 1 21))))))
+
+(defn problem-6 []
   (letfn [(square [x]
             (* x x))]
     (- (square (reduce +
@@ -126,23 +182,65 @@
        (reduce +
                (map square (range 1 101))))))
 
-(def problem-5
-  (reduce *
-          (map (fn [x] (int (apply #(Math/pow %1 %2) x)))
-               (merge-factorizations (map prime-factorization (range 1 21))))))
+;; By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see
+;; that the 6th prime is 13.
+;;
+;; What is the 10 001st prime number?
+(defn problem-7 []
+  (last (take 10001 (primes))))
+
+(defn problem-8 []
+  (let [n (str/replace
+            "73167176531330624919225119674426574742355349194934
+             96983520312774506326239578318016984801869478851843
+             85861560789112949495459501737958331952853208805511
+             12540698747158523863050715693290963295227443043557
+             66896648950445244523161731856403098711121722383113
+             62229893423380308135336276614282806444486645238749
+             30358907296290491560440772390713810515859307960866
+             70172427121883998797908792274921901699720888093776
+             65727333001053367881220235421809751254540594752243
+             52584907711670556013604839586446706324415722155397
+             53697817977846174064955149290862569321978468622482
+             83972241375657056057490261407972968652414535100474
+             82166370484403199890008895243450658541227588666881
+             16427171479924442928230863465674813919123162824586
+             17866458359124566529476545682848912883142607690042
+             24219022671055626321111109370544217506941658960408
+             07198403850962455444362981230987879927244284909188
+             84580156166097919133875499200524063689912560717606
+             05886116467109405077541002256983155200055935729725
+             71636269561882670428252483600823257530420752963450"
+            #"\s"
+            "")
+
+        digits (vec (map #(Integer/parseInt (.toString %)) n))
+        groups-of-13-digits (map #(subvec digits % (+ % 13))
+                                 (range (- (count digits) 12)))]
+    (apply max (map (partial reduce *)
+                    groups-of-13-digits))))
+
+  (defn problem-9 [] "todo")
+  (defn problem-10 [] "todo")
 
 
-;============================================================================
-; Main (just prints out solutions)
-;============================================================================
+  ;;============================================================================
+  ;; Main (just prints out solutions)
+  ;;============================================================================
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (let [problems [["Multiples of 3 and 5" problem-1]
-                  ["Even Fibonacci numbers" problem-2]
-                  ["Largest prime factor" problem-3]
-                  ["Sum square difference" problem-4]
-                  ["Smallest multiple" problem-5]]]
-    (doseq [p problems]
-      (println (first p) (second p)))))
+  (defn -main
+    "I don't do a whole lot ... yet."
+    [& args]
+    (let [problems [["Multiples of 3 and 5" problem-1]
+                    ["Even Fibonacci numbers" problem-2]
+                    ["Largest prime factor" problem-3]
+                    ["Largest palindrome product" problem-4]
+                    ["Smallest multiple" problem-5]
+                    ["Sum square difference" problem-6]
+                    ["10001st prime" problem-7]
+                    ["Largest product in a series" problem-8]
+                    ["Special Pythagorean triplet" problem-9]
+                    ["Summation of primes" problem-10]]]
+      (doseq [problem problems]
+        (let [soln (problem)]
+          (println (first soln) (second soln))))))
