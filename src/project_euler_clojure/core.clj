dd78e4d662578e411e13335a32dd4f1bf81d9f94
Michael Mackenzie <mike.a.mackenzie@gmail.com>
2019-03-11 22:02:21 -0500

    problems 9, 10, 11 and start of new prime function

--- src/project_euler_clojure/core.clj
+++ src/project_euler_clojure/core.clj
@@ -9,7 +9,8 @@
 (ns project-euler-clojure.core
   (:require [clojure.math.combinatorics :as combo]
             [clojure.tools.namespace.repl :refer [refresh]]
-            [clojure.string :as str])
+            [clojure.string :as str]
+            [clojure.set :as zet])
   (:gen-class))
 
 
@@ -50,6 +51,24 @@
           (filter prime?
                   (iterate (partial + 2)
                            3)))))
+
+(defn build-sieve
+  ([n] (build-sieve n 2 #{}))
+
+  ([n i curr-primes]
+   (if (>= i n)
+     curr-primes
+     (if (not (contains? curr-primes i))
+       (recur n (inc i) (into curr-primes
+                              (take-nth i (range n))))
+       (recur n (inc i) curr-primes)))))
+
+
+
+
+(defn eratosthenes []
+
+  (println "hola"))
 
 (defn logarithm
   "Compute logarithm of x in the given base"
@@ -124,13 +143,29 @@
   90009 is a palindrome, but 123 is not."
   [x]
   (case x
-    0 true                                                  ;; avoid division by 0
+    ;; avoid division by 0
+    0 true
+
     (let [n-digits (digit-count x)]
       (every?
         true?
         (map (fn [y]
                (= (digit x y) (digit x (- (dec n-digits) y))))
              (range n-digits))))))
+
+(defn triangle-number [degree]
+  (reduce + (range 1 (inc degree))))
+
+(defn pow [base exp] (int (Math/pow base exp)))
+
+(defn all-factors [x]
+  ;; todo: make this more efficient by using recursion and memoizing
+  (letfn [(ftors [x] (map (memoize #(pow (first x) (inc %))) (range (second x))))]
+    (conj (vec (cons 1
+                     (mapcat identity
+                             (map ftors (prime-factorization x)))))
+          x)))
+
 
 ;;============================================================================
 ;; Solutions to problems
@@ -220,27 +255,89 @@
     (apply max (map (partial reduce *)
                     groups-of-13-digits))))
 
-  (defn problem-9 [] "todo")
-  (defn problem-10 [] "todo")
+;; A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+;;
+;; a2 + b2 = c2
+;; For example, 32 + 42 = 9 + 16 = 25 = 52.
+;;
+;; There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+;; Find the product abc.
+(defn problem-9 []
+  (let [a-and-b (filter #(< (+ (first %) (second %)) 1000)
+                        (combo/cartesian-product (range 1 1000) (range 1 1000)))
+        a-b-c (map (fn [x] [(first x) (second x) (- 1000 (first x) (second x))])
+                   a-and-b)
+        sqr (fn [x] (* x x))]
+
+    (apply *
+           (first
+             (filter #(= (+ (sqr (first %)) (sqr (second %))) (sqr (get % 2)))
+                     a-b-c)))))
+
+(defn problem-10 []
+  (reduce + (take-while (partial > 2000000) (primes))))
+
+(defn problem-11 []
+  (let [v (map #(Integer/parseInt %)
+               (str/split
+                 "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
+                  49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
+                  81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
+                  52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
+                  22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80
+                  24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50
+                  32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70
+                  67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21
+                  24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72
+                  21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95
+                  78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92
+                  16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57
+                  86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58
+                  19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40
+                  04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66
+                  88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69
+                  04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36
+                  20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
+                  20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
+                  01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48"
+                 #"\s+"))
+        indeces (filter #(and (>= 16 (mod % 20)) (>= 336 %)) (range 400))
+        adjacents [[0 1 2 3] [20 21 22 23] [40 41 42 43] [60 61 62 63]
+                   [0 20 40 60] [1 21 41 61] [2 22 42 62] [3 33 43 63]
+                   [0 21 42 63] [60 41 22 3]]
+
+        submatrix (fn [i]
+                    (map (fn [j] (map #(+ i %) j))
+                         adjacents))
+
+        submatrices (mapcat identity
+                            (map #(submatrix %) indeces))]
+
+    (map #(mapv (vec v) %) submatrices)))
+
+(defn problem-12
+  [])
 
 
-  ;;============================================================================
-  ;; Main (just prints out solutions)
-  ;;============================================================================
 
-  (defn -main
-    "I don't do a whole lot ... yet."
-    [& args]
-    (let [problems [["Multiples of 3 and 5" problem-1]
-                    ["Even Fibonacci numbers" problem-2]
-                    ["Largest prime factor" problem-3]
-                    ["Largest palindrome product" problem-4]
-                    ["Smallest multiple" problem-5]
-                    ["Sum square difference" problem-6]
-                    ["10001st prime" problem-7]
-                    ["Largest product in a series" problem-8]
-                    ["Special Pythagorean triplet" problem-9]
-                    ["Summation of primes" problem-10]]]
-      (doseq [problem problems]
-        (let [soln (problem)]
-          (println (first soln) (second soln))))))
+;;============================================================================
+;; Main (just prints out solutions)
+;;============================================================================
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (let [problems [["Multiples of 3 and 5" problem-1]
+                  ["Even Fibonacci numbers" problem-2]
+                  ["Largest prime factor" problem-3]
+                  ["Largest palindrome product" problem-4]
+                  ["Smallest multiple" problem-5]
+                  ["Sum square difference" problem-6]
+                  ["10001st prime" problem-7]
+                  ["Largest product in a series" problem-8]
+                  ["Special Pythagorean triplet" problem-9]
+                  ["Summation of primes" problem-10]
+                  ["Largest product in a grid" problem-11]]]
+    (doseq [problem problems]
+      (let [soln (problem)]
+        (println (first soln) (second soln))))))
